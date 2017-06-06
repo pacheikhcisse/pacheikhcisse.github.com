cbb9b8559397ef3fe3e8cdd67af5a4fa2b7087d4
pacheikhcisse <pacheikhcisse@gmail.com>
2017-06-06 12:49:11 +0000

    add forgotten assets

---
+++ djotali/assets/js/index.js
@@ -0,0 +1,1 @@
+"use strict";$(window).load(function(){$("#preloader").delay(400).fadeOut("slow",function(){AOS.init({easing:"ease-in-out-sine"})}),$("#preloader .clock").fadeOut()}),document.addEventListener("DOMContentLoaded",function(){$(".carousel-single-nav").owlCarousel({loop:!1,margin:0,nav:!0,autoplay:!0,autoplayHoverPause:!0,autoHeight:!1,items:1,dots:!1,navText:["",""],rewind:!0})});
